db590e8aef181728150815959bb5ff56572dc0c4
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-17 15:49:28 +0200

    implemented needed elements

--- popup.js
+++ popup.js
@@ -2,5 +2,13 @@
 chrome.tabs.executeScript( {
   code: "window.getSelection().toString();"
 }, function(selection) {
-  document.getElementById("input").innerHTML = selection[0];
+
+      let input = selection[0];
+      const input_textarea = document.getElementById("input");
+      input_textarea.innerHTML = input;
+      const check_btn = document.getElementById("check_btn");
+      const translate_btn = document.getElementById("translate_btn");
+      const copy_btn = document.getElementById("copy_btn");
+      
 });
+
