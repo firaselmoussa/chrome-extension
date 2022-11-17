76e6f2fe71905b614b2ab2fd0c35ca565b805485
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-17 14:51:01 +0200

    implemented correction functionality

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import langid
+from textblob import TextBlob
 
 sample = "hello world!"
-sample1 = "hello worldd"
+sample1 = "helloo worldd"
 
 
 def detectLanguage(input):
     return langid.classify(input)[0]
 
 
+def check_correct(input):
+    blob = TextBlob(input)
+    return blob.correct()
+
+
 print(detectLanguage(sample))
+print(check_correct(sample1))
